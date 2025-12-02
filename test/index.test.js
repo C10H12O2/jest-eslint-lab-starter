e149ec74ae1513d5293f37ac5e823b0df71b817d
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2025-12-03 00:44:26 +0300

    Addeed jest test to validate the capitalizeWords(input) function

--- test/index.test.js
+++ test/index.test.js
@@ -1,2 +1,20 @@
 const { capitalizeWords, filterActiveUsers, logAction } = require('../index')
 
+describe('capitalizeWords', () => {
+    test("capitalizes the first letter of each word", () => {
+        expect(capitalizeWords("hello world")).toBe("Hello World")
+    });
+
+    test("returns empty string when given empty string", () => {
+        expect(capitalizeWords("")).toBe("")
+    })
+
+    test("handles single word strings", () => {
+        expect(capitalizeWords("javascript")).toBe("Javascript")
+    });
+
+    test("handles special characters", ()=> {
+        expect(capitalizeWords("hello-world")).toBe("Hello-World");
+    });
+});
+
